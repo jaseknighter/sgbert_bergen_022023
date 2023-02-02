810493ad6a4642bb9f55ece6957f4ec7130d04c4
jaseknighter <52048666+jaseknighter@users.noreply.github.com>
2023-02-02 11:39:59 -0800

    wrms params updates

--- python/code/t8.py
+++ python/code/t8.py
@@ -213,8 +213,8 @@ caps[7].append(SensorParam(7, 0, 1, 2, increasing=True))
 caps[8].append(SensorParam(8, 1, 127, 250, increasing=True))
 caps[9].append(SensorParam(9, 1, 2, 0.01, increasing=True))
 # caps[9].append(SensorParam(9, 1, 4, 1, increasing=True, whole_num=True))
-caps[10].append(SensorParam(10, 0, 0.5, 5, increasing=True))
-caps[11].append(SensorParam(11, 0, 0.5, 5, increasing=True))
+caps[10].append(SensorParam(10, 0.05, 0.5, 5, increasing=True))
+caps[11].append(SensorParam(11, 0.05, 0.5, 5, increasing=True))
 
 # knobs_to_osc = defaultdict(list)
 # IMPORTANT: set range in the first line below to the number of knobs you are defining 
@@ -421,8 +421,8 @@ while True:
                           elif path == "/transient":
                             client.send_message("/transient", 0)
                           elif path == "/old 1" or  path == "/old 2":
-                            client.send_message("/params/vol 1", 0)
-                            client.send_message("/params/vol 2", 0)
+                            client.send_message("/params/vol 1", 0.05)
+                            client.send_message("/params/vol 2", 0.05)
                             client.send_message("/params"+path, minval)
                           elif path:
                             client.send_message("/params"+path, minval)
