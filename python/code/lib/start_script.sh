87da412db25fd2852d6dc49b766e06003edd1db0
jaseknighter <52048666+jaseknighter@users.noreply.github.com>
2023-02-06 10:52:31 -0800

    move start script

--- python/code/lib/start_script.sh
+++
@@ -1,5 +0,0 @@
-sudo pkill -f t7_single_cap_touch.py
-cd /home/pi/touched/bergen_res_2022/code/ && python t7_single_cap_touch.py
-
-#cd /home/pi/touched/bergen_res_2022/code/ && python t7_single_cap_touch.py
-#cd ~/touched/bergen_res_2022/code/ && python t7_single_cap_touch.py >> ~/touched/bergen_res_2022/code/bergen_script.log
---
+++ python/code/start_script.sh
@@ -0,0 +1,8 @@
+sudo pkill -f t9.py
+cd /home/pi/touched/bergen_res_2022/code/ && python t9.py
+
+# replace  line 2 with line below one to skip midi sequence
+# cd /home/pi/touched/bergen_res_2022/code/ && python t9.py  --skipMidiSequence true
+
+# replace  line 2 with line below one to add logging
+#cd /home/pi/touched/bergen_res_2022/code/ && python t9.py >> /home/pi/touched/bergen_res_2022/code/bergen_script.log
